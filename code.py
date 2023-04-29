ce0300ade6b836bc640a8e366df41ed7363bd6b2
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2023-04-29 13:05:38 -0400

    Update code.py

--- code.py
+++ code.py
@@ -12,6 +12,7 @@ def boucle_principale():
     while True:
         # Déclare les variables pour la boucle
         temperature = dht.temperature
+        humidite = dhtDevice.humidity
         val_bouton = bouton.value
         
         # Allume ou eteindre la lumière
@@ -22,10 +23,17 @@ def boucle_principale():
         
         # Quand on lache le bouton,
         if val_bouton == 0 and val_bouton_dernere_boucle == 1:
+            #compte le nombre d'ouverture de la porte
             nb_ouverture += 1
             porte_ouverte = not porte_ouverte
             # Ouvre/ferme la porte
             controle_porte(porte_ouverte)
+        
+        # Vérifie le niveau d'eau dans le reservoire
+        pass
+        
+        # Vérifie si l'humiditée des légumes sont bonnes
+        
         
         # Affichage : Entrée = temperature, niveau d'eau ; Sortie = moteur, nb d'ouverture de porte.
         affichage(temperature)
