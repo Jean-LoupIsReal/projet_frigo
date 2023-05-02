ce5f3fa69ae7797b7d25853f3b36eb320cf2a38a
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2023-05-02 12:40:36 -0400

    Update fonctions.py
    final?

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,5 @@
 from init_projet import *
-import simpleio
+
 #========================================== Fonction du wifi ==========================================
 
 # Fonction qui gère l'envois de l'information à internet
@@ -30,27 +30,24 @@ def music_zelda():
             523]
     
     for i in range(len(melodie)):
-        simpleio.tone(pin_buzzer, melodie[i], duration=0.15)
+        simpleio.tone(pin_buzzer, melodie[i], duration=0.15) 
         
-# Vérifie si l'on doit aroser les légumes.
+# Vérifie si l'on doit arroser les fruits et les légumes.
 def verification_humidite(humidite):
     if humidite < 55:
-                # Arose les légumes. (buzzer pour simulation)
                 spray_legumes()
                 
-# Arose les légumes. (simuler par buzzer)
+# Arrose les fruits et les légumes. (buzzer pour simulation)
 def spray_legumes():
-        simpleio.tone(pin_buzzer, 349, duration=0.15)
+        simpleio.tone(pin_buzzer, 349, duration=0.3)
         
         
 #========================================== Fonctions de la porte ==========================================
 
-# Ouvre la porte du frigot.
+# Ouvre la porte du frigo.
 def ouverture_porte():
     porte.angle = 120
 
-    
-    
 # Ferme la porte du frigot.
 def fermeture_porte():
     porte.angle = 0
@@ -60,18 +57,25 @@ def controle_porte(porte_ouverte):
     #Transforme l'ouverture de la porte en bool
     if porte_ouverte:
         ouverture_porte()
+        # Allume la lumière
+        led.color = (0,0,0)
         music_zelda()
+        # Retoune une valeur pour compter le nombre d'ouverture.
+        return 1
     else:
         fermeture_porte()
-
+        # Ferme la lumière.
+        led.color = (255,255,255)
+        # Retoune une valeur pour compter le nombre d'ouverture.
+        return 0
 
 #========================================== Fonctions du moteur (refroidissement) ==========================================
 
-# Vérifie si le frigo est assé froid.
+# Vérifie si le frigo est assez froid.
 def refroidissement(temp):
-    # donne une valeur de 0 a 1 selon la temperature 
+    # Donne une valeur de 0 a 1 selon la temperature 
     val_moteur = (temp - 5) /5
-    #s'assure que la valeur soit entre 0 et 1
+    # S'assure que la valeur soit entre 0 et 1
     if val_moteur < 0:
         val_moteur = 0
     elif val_moteur > 1:
@@ -82,24 +86,27 @@ def refroidissement(temp):
 #========================================== Fonctions du reservoir ==========================================
 def pourcent_capt(val_capt):
     # Normaliser en % la valeur du capteur
-    pass
-    
+    pourcent_eau = round((val_capt - 30000)/19700 * 20) * 5
+    if pourcent_eau < 0:
+        pourcent_eau = 0
+    return pourcent_eau 
 
 # Utilise la pompe pour remplir le reservoir d'eau jusqu'à 100%.
 def remplir_reservoir(pourcent_capt):
     # Tant qu'elle n'arrive pas 100%, continuer de remplir le reservoir 
     if pourcent_capt < 100:
-        #
-        pass
+        pompe.value = True
+        return True
     else:
-        pass
-
+        pompe.value = False
+        return False
 
 #========================================== Fonctions de l'affichage ==========================================
 
 # Fonction qui affiche les information a l'écran
-def affichage(temp):
-    pass
-
-
-#========================================== Fonctions bonus ==========================================
+def affichage(temp, moteur,nb_ouverture, reservoir):
+    text = "Temperature = {0} \nMoteur = {1}% \nNb ouverture = {2} \nReservoir = {3}%".format(temp, moteur, nb_ouverture, reservoir)
+    text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=2)
+    text_area.x = 10
+    text_area.y = 20
+    board.DISPLAY.show(text_area)
