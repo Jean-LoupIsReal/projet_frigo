564ef719889954e8c340ce051d0aee0c48459bfe
Dydy <119439596+DydyKH@users.noreply.github.com>
2023-05-01 21:59:51 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 # pyright: reportShadowedImports=false
-# Contien toutes les imports et les déclarations
+# Contient tous les imports et les déclarations
 from init_projet import *
 from fonctions import *
 
 # Fonction de la boucle principale.
 def boucle_principale():
     # Déclare les variables necessaire a la boucle.
-    val_bouton_dernere_boucle = 0
+    val_bouton_dernier_boucle = 0
     nb_ouverture = 0
     porte_ouverte = False
     led.color = (255,255,255)
@@ -25,11 +25,11 @@ def boucle_principale():
         
         val_bouton = bouton.value
 
-        # Vérifie si le frigot doit etre refroidi
+        # Vérifie si le frigo doit etre refroidi
         pourcent_moteur = refroidissement(temperature)
         
         # Quand on lache le bouton,
-        if val_bouton == 0 and val_bouton_dernere_boucle == 1:
+        if val_bouton == 0 and val_bouton_dernier_boucle == 1:
             porte_ouverte = not porte_ouverte
             # Ouvre/ferme la porte et compte le nombre d'ouverture de la porte
             nb_ouverture += controle_porte(porte_ouverte)
@@ -43,11 +43,11 @@ def boucle_principale():
             remplissage = True
         
         if remplissage:
-            #Rempli le reservoir
+            # Rempli le reservoir
             remplissage = remplir_reservoir(reservoir, remplissage)
         
     
-        # Vérifie si l'humiditée des légumes est bonne
+        # Vérifie si l'humidité est bonne
         if timer_spray + 10 <= time.monotonic():
             verification_humidite(humidite)
             timer_spray = time.monotonic()
@@ -61,7 +61,7 @@ def boucle_principale():
         #envoisWifi(temperature)
         
         # Garde en memoire la valeur du bouton 
-        val_bouton_dernere_boucle = val_bouton
+        val_bouton_dernier_boucle = val_bouton
         
         # empeche de bruler l'esp
         time.sleep(0.01)
